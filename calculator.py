b3638a506b8a45bd82cb83d5d995937c3de8b549
Imran <imranc42@gmail.com>
2024-09-16 13:47:14 -0500

    added functions and a squareroot function

--- calculator.py
+++ calculator.py
@@ -1,1 +1,35 @@
-print ("test")
+
+def add(x, y):
+    """Addition"""
+    return x + y
+
+def subtract(x, y):
+    """Subtraction"""
+    return x - y
+
+def multiply(x, y):
+    """Multiplication"""
+    return x * y
+
+def divide(x, y):
+    """Division"""
+    if y == 0:
+        raise ValueError("Cannot divide by zero!")
+    return x / y
+
+def square_root(x):
+    """Square Root"""
+    if x < 0:
+        raise ValueError("Cannot take square root of a negative number!")
+    return math.sqrt(x)
+  
+if __name__ == "__main__":
+    # Sample inputs
+    a = 16
+    b = 4
+
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {subtract(a, b)}")
+    print(f"{a} * {b} = {multiply(a, b)}")
+    print(f"{a} / {b} = {divide(a, b)}")
+    print(f"Square root of {a} = {square_root(a)}")
